d0fae48e4b8faecefc62528a33f12e2fae172cf9
DrewVinciguerra <126896614+DrewVinciguerra@users.noreply.github.com>
2023-03-06 16:16:36 -0700

    Basic framework for processing a next turn

--- Clash2023/Creature.h
+++ Clash2023/Creature.h
@@ -24,11 +24,22 @@ private:
 	//I should be able to use a forward dec if I really want to use this.
 	//HexBoardSpace* p_location;	// Where this creature is currently located
 
-	// I need a way to mark a target for this creature to travel to and/or  attack
-	int main_attack_target;
+	// I need a way to mark a target for this creature to travel to and/or attack
+	// A creature could either want to travel to the other player, a specific creature or just a hex.
+	int player_target_id;
+	int creature_target_id;
+	int hex_target_id;
 
 	int id;					// This creature's id
+	
+	//Hex grid location
+	int x;	
+	int y;
 
+public:
+	Creature(std::string name, int health, int attack_range_close, int attack_range_far, int movement, int controler_id, int player_target_id, int x, int y);
+	~Creature();
+	std::string Name();
 };
 
 /*Beholder
